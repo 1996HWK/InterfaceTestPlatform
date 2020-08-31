4f5dfd7483e156a5c0e9faf59398a1a45ca7a802
HuangWeiKai <15766126202@163.com>
2020-08-31 15:53:58 +0800

    新增了公共方法，获取文件绝对路径

---
+++ common/public.py
@@ -0,0 +1,10 @@
+# 返回文件路径
+import os
+
+
+def file_abs_path(file_name):
+    """
+       返回文件绝对路径
+    """
+    return os.path.join(os.path.dirname(os.path.dirname(__file__)),"data", str(file_name))
+
