52f2be8f75a9799e3d0555fee2366b410a463049
黄炜恺 <15766126202@163.com>
2020-09-19 09:08:26 +0800

    操作方法新增了对应的excel的字符串

--- utils/OperationExcel.py
+++ utils/OperationExcel.py
@@ -3,6 +3,17 @@ import xlrd
 from OperationIni import OperationIni
 
 
+class VarName(object):
+    Title = "测试模块"
+    Run = "执行"
+    URl = "请求地址"
+    Method = "请求方法"
+    Header = "请求头"
+    Data = "请求内容"
+    Assertion = "断言"
+    SaveDate = "保存数据"
+
+
 class Excel(object):
 
     def __init__(self):
@@ -23,8 +34,18 @@ class Excel(object):
             test_all_case.append(dict(zip(title, value)))
         return test_all_case
 
+    def get_case(self):
+        """ 返回执行的测试用例 """
+        data = self.get_all_data()
+        test_all = []
+        for d in data:
+            if d[VarName.Run] == "turn":
+                test_all.append(d)
+
+        return test_all
+
 
 if __name__ == '__main__':
     excel = Excel()
-    excel.get_all_data()
+    print(excel.get_case())
 
